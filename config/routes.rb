201f2c56d55464c06459f4ef238210c44b5772e7
Chris White <chris.white@thoughtbot.com>
2023-08-11 13:24:43 -0600

    rails generate react_on_rails:install

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'hello_world', to: 'hello_world#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
